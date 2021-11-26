013504f933ac31e58df728ae13b504d5b791b0c3
bhuvi00700 <93527546+bhuvi00700@users.noreply.github.com>
2021-11-26 15:07:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function User(firstName, lastName, dateOfBirth) {
+function Person(firstName, lastName, dateOfBirth) {
   this.firstName = firstName;
   this.lastName = lastName;
   this.dateOfBirth = dateOfBirth;
 }
 
-var person = new User("John", "Smith", 1985);
-var person_two = new User("Jim", "Sean", 1995);
-var person_three = new User("Tom", "Cruise", 1974);
+var person = new Person("John", "Smith", 1985);
+var person_two = new Person("Jim", "Sean", 1995);
+var person_three = new Person("Tom", "Cruise", 1974);
 
 console.log(person);
 console.log(person_two);
